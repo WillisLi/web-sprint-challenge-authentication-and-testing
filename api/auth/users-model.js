46fa8b04f5035e73c140c7b2c9ebba9c574cb809
WillisLi <li.willis95@gmail.com>
2021-09-16 17:27:23 -0700

    completed user-model db helper functions

---
+++ api/auth/users-model.js
@@ -0,0 +1,25 @@
+const db = require('../../data/db-config.js');
+
+function get() {
+    return db('users');
+}
+
+function getBy(filter) {
+    return db('users').where(filter).orderBy('id')
+}
+
+function getById(id) {
+    return db('users').where('id', id).first();
+}
+
+async function add(user) {
+    const [id] = await db('users').insert(user);
+    return getById(id);
+}
+
+module.exports = {
+    get,
+    getById,
+    getBy,
+    add,
+}
