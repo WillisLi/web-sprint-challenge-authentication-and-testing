6aed4c30b20b01af4efd21e2cd1aac838df689db
WillisLi <li.willis95@gmail.com>
2021-09-16 19:45:53 -0700

    completed tests for logina and register endpoints

--- api/server.test.js
+++ api/server.test.js
@@ -16,7 +16,7 @@ afterAll(async () => {
 })
 
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
 })
 
 describe('[POST] /api/auth/register', () => {
@@ -24,12 +24,27 @@ describe('[POST] /api/auth/register', () => {
     const res = await request(server)
       .post('/api/auth/register')
       .send({ username: 'hey', password: 'wefredgb' })
-    expect(res.body).toMatchObject({ id: 1, username: 'hey' })
+    expect(res.body).toMatchObject({ id: 3, username: 'hey' })
   }, 600)
-  test('responds with a 422 on missing username', async () => {
+  test('responds with a 422 on missing password', async () => {
     const res = await request(server)
       .post('/api/auth/register')
-      .send({ usrname: 'heey' })
+      .send({ username: 'heey' })
     expect(res.status).toBe(422)
+  }, 600)
+})
+
+describe('[POST] /api/auth/login', () => {
+  test('responds with the user info on successful login', async () => {
+    const res = await request(server)
+      .post('/api/auth/login')
+      .send({ username: 'bob', password: '1234' })
+    expect(res.body).toMatchObject({message: "welcome, bob!"})
+  }, 600)
+  test('responds with a 401 on bad credentials', async () => {
+    const res = await request(server)
+      .post('/api/auth/login')
+      .send({ username: 'wefr', password: '1234' })
+    expect(res.status).toBe(401)
   }, 600)
 })
